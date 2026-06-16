f20c6cf592e046e758e3727fde50003dfb51c273
youssefahmed505 <cananyonepvpme2@gmail.com>
2026-06-16 04:10:40 +0300

    Create App.js

---
+++ src/src/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="content-overlay">
+        <h1>Welcome to Paradise Nursery</h1>
+        <p>Where Green Meets Serenity</p>
+        <button className="get-started-btn">Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
